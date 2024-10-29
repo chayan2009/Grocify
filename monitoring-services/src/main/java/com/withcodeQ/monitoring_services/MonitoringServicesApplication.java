714b7f029bf1300f5865f1d2db9e42427bc0ebc1
chayan <chayanchowdhury@gmail.com>
2024-10-29 09:09:20 +0530

    Grafana and Prometheus monitoring project for Spring Boot

---
+++ monitoring-services/src/main/java/com/withcodeQ/monitoring_services/MonitoringServicesApplication.java
@@ -0,0 +1,13 @@
+package com.withcodeQ.monitoring_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServicesApplication.class, args);
+	}
+
+}
